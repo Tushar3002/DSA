26e7336936f352001948cccde214f10dc4a1e0a0
Tushar <tusharjana.aj3@gmail.com>
2025-07-30 23:58:02 +0530

    NQueen

--- leetcode/leet51_NQueens.cpp
+++ leetcode/leet51_NQueens.cpp
@@ -1,66 +1,82 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-bool isSafe(vector<string> &board,int row,int col,int n){
-        //hori
-        for(int j=0;j<n;j++){
-            if(board[row][j]=='Q'){
-                return false;
-            }
-        }
-
-        //vert
-        for(int i=0;i<n;i++){
-            if(board[i][col]=='Q'){
-                return false;
-            }
-        }
-
-        //left diagonal
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-
-        //right dia
-        for(int i=row,j=col;i>=0 && j<n;i--,j++){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-        return true;
-
-    }
-    void nqueen(vector<string> &board,int row,int n,vector<vector<string>> &ans){
-        if(row==n){
-            ans.push_back({board});
-            return;
-        }
-        for(int j=0;j<n;j++){
-            if(isSafe(board,row,j,n)){
-                board[row][j]='Q';
-                nqueen(board,row+1,n,ans);
-                board[row][j]='.';
-            }
+bool isSafe(vector<string> &board, int row, int col, int n)
+{
+    // hori
+    for (int j = 0; j < n; j++)
+    {
+        if (board[row][j] == 'Q')
+        {
+            return false;
         }
     }
-    vector<vector<string>> solveNQueens(int n) {
-           vector<vector<string>> ans;
-           vector<string> board(n,string(n,'.'));
 
-           nqueen(board,0,n,ans);
-           return ans;
-    }
-
-    int main(){
-        vector<vector<string>> ans=solveNQueens(7);
-        for(const auto& i:ans){
-            for (const auto& j:i){
-                cout << j <<endl;
-            }
-            cout << endl;
+    // vert
+    for (int i = 0; i < n; i++)
+    {
+        if (board[i][col] == 'Q')
+        {
+            return false;
         }
-        return 0;
     }
+
+    // left diagonal
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+    }
+
+    // right dia
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+void nqueen(vector<string> &board, int row, int n, vector<vector<string>> &ans)
+{
+    if (row == n)
+    {
+        ans.push_back({board});
+        return;
+    }
+    for (int j = 0; j < n; j++)
+    {
+        if (isSafe(board, row, j, n))
+        {
+            board[row][j] = 'Q';
+            nqueen(board, row + 1, n, ans);
+            board[row][j] = '.';
+        }
+    }
+}
+vector<vector<string>> solveNQueens(int n)
+{
+    vector<vector<string>> ans;
+    vector<string> board(n, string(n, '.'));
+
+    nqueen(board, 0, n, ans);
+    return ans;
+}
+
+int main()
+{
+    vector<vector<string>> ans = solveNQueens(5);
+    for (const auto &i : ans)
+    {
+        for (const auto &j : i)
+        {
+            cout << j << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
